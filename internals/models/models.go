6b0d83314dbabe1eaacdcada0d245d38f18180a6
Felipe Frizzo <felipefrizzo@gmail.com>
2020-03-15 18:49:53 -0300

    Create struct to unmarshal soap request

--- internals/models/models.go
+++ internals/models/models.go
@@ -6,15 +6,20 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// SOAPResponse struct has information to parse xml return
+type SOAPResponse struct {
+	Address Address `xml:"Body>consultaCEPResponse>return"`
+}
+
 // Address struct has information from brazilian addresses
 type Address struct {
 	ID             bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-	FederativeUnit string        `json:"federative_unit" bson:"federative_unit"`
-	City           string        `json:"city" bson:"city"`
-	Neighborhood   string        `json:"neighborhood" bson:"neighborhood"`
-	AddressName    string        `json:"address_name" bson:"address_name"`
-	Complement     string        `json:"complement" bson:"complement"`
-	Zipcode        int32         `json:"zipcode" bson:"zipcode"`
+	FederativeUnit string        `json:"federative_unit" bson:"federative_unit" xml:"uf"`
+	City           string        `json:"city" bson:"city" xml:"cidade"`
+	Neighborhood   string        `json:"neighborhood" bson:"neighborhood" xml:"bairro"`
+	AddressName    string        `json:"address_name" bson:"address_name" xml:"end"`
+	Complement     string        `json:"complement" bson:"complement" xml:"complemento2"`
+	Zipcode        int32         `json:"zipcode" bson:"zipcode" xml:"cep"`
 	CreatedAt      time.Time     `json:"created_at" bson:"created_at"`
 	UpdatedAt      time.Time     `json:"updated_at" bson:"updated_at"`
 }
