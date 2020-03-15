ab9e0edba77da1d83311bfcbc7ec56f49a8616d6
Felipe Frizzo <felipefrizzo@gmail.com>
2020-03-15 18:50:53 -0300

    Create function to abstract xml request

---
+++ internals/abstract/request.go
@@ -0,0 +1,26 @@
+package abstract
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+// RequestXML abstraction function to make HTTP request in XML
+func RequestXML(url string, body string, fields []interface{}) (*http.Response, error) {
+	client := &http.Client{}
+	payload := []byte(fmt.Sprintf(body, fields...))
+
+	request, err := http.NewRequest("POST", url, bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/soap+xml; charset=iso-8859-1")
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
