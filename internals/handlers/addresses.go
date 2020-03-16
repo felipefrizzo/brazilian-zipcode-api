fbe9d9a394a547f54925b98871cdfc88c2be96c1
Felipe Frizzo <felipefrizzo@gmail.com>
2020-03-16 08:15:43 -0300

    Improve handler errors and call function to validate if address is updated

--- internals/handlers/addresses.go
+++ internals/handlers/addresses.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -14,19 +15,31 @@ import (
 // ZipcodeHandler function for returns the address corresponding to the zipcode
 func ZipcodeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var params = mux.Vars(r)
+	var params map[string]string = mux.Vars(r)
 	var address models.Address
 
 	zipcode, err := strconv.Atoi(params["zipcode"])
 	if err != nil {
-		panic(err)
+		log.Printf("ZIPCODE_HANDLER_ERROR - Error to convert the zipcode parameter - %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
 
 	session, err := middleware.MongoConnection()
 	if err != nil {
-		panic(err)
+		log.Printf("ZIPCODE_HANDLER_ERROR - Error to close connection with MongoDB - %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
 
 	session.DB("zipcode").C("addresses").Find(bson.M{"zipcode": zipcode}).One(&address)
+	err = address.AddressIsUpdated(zipcode)
+	if err != nil {
+		log.Printf("ZIPCODE_HANDLER_ERROR - Invalid address - %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	session.DB("zipcode").C("addresses").Upsert(bson.M{"zipcode": zipcode}, address)
+
 	json.NewEncoder(w).Encode(address)
 }
