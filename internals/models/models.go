a29f3ae741fb9162d7f47c38e271c0de2ae7886d
Felipe Frizzo <felipefrizzo@gmail.com>
2020-03-16 08:15:00 -0300

    Create function to validate if the address is updated

--- internals/models/models.go
+++ internals/models/models.go
@@ -1,10 +1,29 @@
 package models
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/felipefrizzo/brazilian-zipcode-api/internals/abstract"
+	"github.com/felipefrizzo/brazilian-zipcode-api/internals/configs"
+	"github.com/felipefrizzo/brazilian-zipcode-api/internals/helpers"
 	"gopkg.in/mgo.v2/bson"
 )
+
+const body string = `
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cli="http://cliente.bean.master.sigep.bsb.correios.com.br/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <cli:consultaCEP>
+         <cep>%d</cep>
+      </cli:consultaCEP>
+   </soapenv:Body>
+</soapenv:Envelope>
+`
 
 // SOAPResponse struct has information to parse xml return
 type SOAPResponse struct {
@@ -22,4 +41,70 @@ type Address struct {
 	Zipcode        int32         `json:"zipcode" bson:"zipcode" xml:"cep"`
 	CreatedAt      time.Time     `json:"created_at" bson:"created_at"`
 	UpdatedAt      time.Time     `json:"updated_at" bson:"updated_at"`
+}
+
+// AddressIsUpdated this function is responsible to get the current address and validate if is updated
+// based on UpdatedAt and Correios data
+func (a *Address) AddressIsUpdated(zipcode int) error {
+	if a.Zipcode != 0 {
+		zipcode = int(a.Zipcode)
+	}
+
+	c, err := getAddressByZipcodeCorreios(zipcode)
+	if err != nil {
+		log.Printf("ADDRESS_VALIDATE_ERROR - Something goes wrong with Correios API - %v", err)
+		return fmt.Errorf("Correios API error - %v", err)
+	}
+
+	if a.Zipcode == 0 {
+		log.Printf("ADDRESS_VALIDATE_INFO - Inserting a new address")
+
+		a.FederativeUnit = c.FederativeUnit
+		a.City = c.City
+		a.Neighborhood = c.Neighborhood
+		a.AddressName = c.AddressName
+		a.Complement = c.Complement
+		a.Zipcode = c.Zipcode
+		a.CreatedAt = time.Now().UTC()
+		a.UpdatedAt = time.Now().UTC()
+	}
+
+	sevenDaysAgo := time.Now().UTC().AddDate(0, 0, -7)
+	if a.UpdatedAt.Before(sevenDaysAgo) {
+		log.Printf("ADDRESS_VALIDATE_INFO - The current address is outdated")
+
+		a.FederativeUnit = c.FederativeUnit
+		a.City = c.City
+		a.Neighborhood = c.Neighborhood
+		a.AddressName = c.AddressName
+		a.Complement = c.Complement
+		a.Zipcode = c.Zipcode
+		a.UpdatedAt = time.Now().UTC()
+	}
+
+	return nil
+}
+
+func getAddressByZipcodeCorreios(zipcode int) (*Address, error) {
+	var soap SOAPResponse
+	var URL string = configs.Config.CorreiosURL
+
+	response, err := abstract.RequestXML(URL, body, []interface{}{zipcode})
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Status code: %v", response.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	xml.Unmarshal(helpers.ISO8859ToUTF8(data), &soap)
+
+	return &soap.Address, nil
 }
